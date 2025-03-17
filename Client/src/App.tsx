03ba3fd6ee85c8da56cb9ea4465003cf942dbd49
NeckitWin <neckit.v@gmail.com>
2025-03-17 17:33:33 +0100

    push empty project

---
+++ Client/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="flex justify-center flex-col gap-4 items-center">
+      <div>
+        <a href="https://nubisoft.io/" target="_blank">
+          <img src="svg/nubisoft.svg" className="" alt="Nubisoft logo" />
+        </a>
+      </div>
+      <h1>NubiWeather</h1>
+      <section>
+        <h2>Here's your starting point.</h2>
+        <h3>Good luck! :)</h3>
+      </section>
+    </div>
+  );
+}
+
+export default App;
