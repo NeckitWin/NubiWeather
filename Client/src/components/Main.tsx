a9e310c5f8a995fa7eebd1617e980e8d97804012
NeckitWin <neckit.v@gmail.com>
2025-03-17 18:52:27 +0100

    update: styles navbar & add color theme variables

--- Client/src/components/Main.tsx
+++ Client/src/components/Main.tsx
@@ -1,8 +1,9 @@
 const Main = () => {
     return (
-        <>
+        <main className="bg-background rounded-2xl shadow-md shadow-primary mt-6 p-4">
+            <h1 className='text-center text-primary text-4xl font-bold'>Miasto</h1>
 
-        </>
+        </main>
     );
 }
 
